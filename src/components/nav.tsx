af23152e45589cde30c8e1fdf647c3f1f28e4593
Felix Ramnelöv <felix@ramnelov.com>
2024-06-04 22:34:21 +0200

    Added update to navbar, terrible internet

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,21 +1,39 @@
-import { Component, createSignal } from "solid-js";
+import { Component, createEffect, createSignal } from "solid-js";
 import { A, useLocation } from "@solidjs/router";
 import { FiMenu } from "solid-icons/fi";
 
 export const Nav: Component = () => {
   const [isOpen, setIsOpen] = createSignal(false);
   const location = useLocation();
+  const [navRef, setNavRef] = createSignal<HTMLDivElement | null>(null)
+
+  /**
+   * Handle click outside of the nav to close it
+   * @param e 
+   */
+  const handleOutsideClick = (e: MouseEvent) => {
+    const ref = navRef()
+    if (ref && !ref.contains(e.target as Node)) {
+      setIsOpen(false);
+    }
+  }
+
+  /**
+   * Add or remove event listener for outside click
+   */
+  createEffect(() => {
+    if (isOpen()) {
+      document.addEventListener("click", handleOutsideClick);
+    } else {
+      document.removeEventListener("click", handleOutsideClick);
+    }
+  })
 
   return (
     <>
-      <button
-        onClick={() => {
-          setIsOpen(!isOpen());
-        }}
-      >
-        <FiMenu class="size-14 px-1 py-1 transition-colors duration-100 text-gray-400 hover:text-white" />
-      </button>
+      <FiMenu onClick={() => setIsOpen(!isOpen())} class="size-14 px-1 py-1 transition-colors duration-100 text-gray-400 hover:text-white" />
       <nav
+        ref={setNavRef}
         class={`mt-16 text-3xl font-titillium backdrop-blur fixed border border-l-0 border-gray-300 border-opacity-20 flex flex-col items-start p-4 transition-transform duration-200 ${
           isOpen() ? "translate-x-0" : "translate-x-full"
         }`}
@@ -30,6 +48,16 @@ export const Nav: Component = () => {
           }`}
         >
           Home
+        </A>
+        <A
+          href="/about"
+          class={`transition-colors duration-100 my-2 font-semibold ${
+            location.pathname === "/about"
+              ? "text-white"
+              : "text-gray-400 hover:text-white"
+          }`}
+        >
+          About
         </A>
         <A
           href="/education"
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,11 +5,11 @@ export const Home: Component = () => {
     <div class="flex flex-col items-center justify-center text-center font-titillium">
       <h1 class="text-5xl glow">Welcome!</h1>
       <p class="text-xl">... to my resume website</p>
-      <p class="pt-5 max-w-72">
-        This is a side project of mine, developing a SPA that can be used as a
+      <p class="pt-5 max-w-80">
+        This is a side project of mine, developing a SPA and PWA that can be used as a
         resume.
       </p>
-      <p class="pt-5 max-w-72">
+      <p class="pt-5 max-w-80">
         Feel free to look around, click the menu icon in the top left.
       </p>
     </div>
