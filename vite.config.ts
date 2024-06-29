be6fd9cd8fb39d2930210de060ad654f5955195e
Felix Ramnelöv <felix@ramnelov.com>
2024-06-30 01:12:50 +0200

    main: updated pwa assets

--- vite.config.ts
+++ vite.config.ts
@@ -1,55 +1,51 @@
-import { defineConfig } from "vite";
-import solid from "vite-plugin-solid";
-import path from "path";
-import { VitePWA } from "vite-plugin-pwa";
-import type { VitePWAOptions } from "vite-plugin-pwa";
-import devtools from "solid-devtools/vite";
+import { defineConfig } from 'vite'
+import solid from 'vite-plugin-solid'
+import path from 'path'
+import { VitePWA } from 'vite-plugin-pwa'
+import type { VitePWAOptions } from 'vite-plugin-pwa'
+import devtools from 'solid-devtools/vite'
 
 const pwaOptions: Partial<VitePWAOptions> = {
-  registerType: "autoUpdate",
-  injectRegister: "auto",
+  registerType: 'autoUpdate',
+  injectRegister: 'auto',
   devOptions: {
     enabled: true,
   },
 
-  includeAssets: ["favicon.ico", "apple-touch-icon.png", "mask-icon.svg"],
+  includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
   manifest: {
-    name: "Felix Ramnelöv",
-    short_name: "FR",
-    description: "Resume Website",
-    theme_color: "#000000",
+    name: 'Felix Ramnelöv',
+    short_name: 'Felix Ramnelöv',
+    description: 'Felix Ramnelöv - Resume Website',
+    theme_color: '#000000',
     icons: [
       {
-        src: "pwa-64x64.png",
-        sizes: "64x64",
-        type: "image/png",
+        src: 'pwa-64x64.png',
+        sizes: '64x64',
+        type: 'image/png',
       },
       {
-        src: "pwa-192x192.png",
-        sizes: "192x192",
-        type: "image/png",
+        src: 'pwa-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
       },
       {
-        src: "pwa-512x512.png",
-        sizes: "512x512",
-        type: "image/png",
+        src: 'pwa-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
       },
       {
-        src: "maskable-icon-512x512.png",
-        sizes: "512x512",
-        type: "image/png",
-        purpose: "maskable",
+        src: 'maskable-icon-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+        purpose: 'maskable',
       },
     ],
   },
-};
+}
 
 export default defineConfig({
-  plugins: [
-    solid(),
-    devtools({ autoname: true }),
-    VitePWA(pwaOptions),
-  ],
+  plugins: [solid(), devtools({ autoname: true }), VitePWA(pwaOptions)],
   server: {
     watch: {
       usePolling: true,
@@ -61,10 +57,10 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      "~": path.resolve(__dirname, "./src"),
+      '~': path.resolve(__dirname, './src'),
     },
   },
   preview: {
     port: 8080,
   },
-});
+})
