036d02ef663118ce71894c8c5396eb6507a17834
Felix Ramnelöv <felix@ramnelov.com>
2024-06-30 15:58:37 +0200

    mani: updated label of experience nav

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -44,7 +44,7 @@ export const Nav: Component = () => {
       >
         <NavLink href="/">Home</NavLink>
         <NavLink href="/education">Education</NavLink>
-        <NavLink href="/experience">Experiences</NavLink>
+        <NavLink href="/experience">Experience</NavLink>
         <NavLink href="/under-development">Projects</NavLink>
         <NavLink href="/under-development">Contact</NavLink>
       </nav>
