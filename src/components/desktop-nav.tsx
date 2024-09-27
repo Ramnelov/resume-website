9d07f6aeb6b46131936b05baebc471ac5383cf8e
Felix Ramnelöv <felix@ramnelov.com>
2024-09-27 22:02:25 +0200

    main: small margin fixes

--- src/components/desktop-nav.tsx
+++ src/components/desktop-nav.tsx
@@ -3,7 +3,7 @@ import { NavLink } from '~/components/nav-link'
 
 export const DesktopNav: Component = () => {
   return (
-    <nav class="fixed left-0 right-0 top-0 z-50 mb-5 flex items-center justify-center gap-3 border-b-1 border-gray-300 border-opacity-20 text-3xl font-semibold backdrop-blur">
+    <nav class="fixed left-0 right-0 top-0 z-50 flex items-center justify-center gap-3 border-b-1 border-gray-300 border-opacity-20 text-3xl font-semibold backdrop-blur">
       <NavLink href="/">Home</NavLink>
       <NavLink href="/education">Education</NavLink>
       <NavLink href="/experience">Experience</NavLink>
--- src/components/page-container.tsx
+++ src/components/page-container.tsx
@@ -2,7 +2,7 @@ import type { ParentComponent } from 'solid-js'
 
 const PageContainer: ParentComponent = ({ children }) => {
   return (
-    <div class="mt-20 flex grow flex-col items-center justify-center text-center">
+    <div class="mt-20 flex grow flex-col items-center justify-center pb-3 text-center">
       <div class="max-w-xs md:max-w-md lg:max-w-lg">{children}</div>
     </div>
   )
